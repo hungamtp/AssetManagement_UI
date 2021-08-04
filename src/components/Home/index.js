6a09464f906a203b178d628f3fd8191e424364cd
CuongNC <you@example.com>
2021-08-04 22:54:01 +0700

    initial part 3: fix constant in home page demo

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,14 +2,15 @@ import React, { Component } from "react";
 import "./Home.css";
 import Navbar from "../Navbar";
 import Menu from "../Menu";
+import * as business from "../../constants/Business";
 
 export default class index extends Component {
   render() {
     return (
       <div id="Home">
         <Navbar businessName="Name of you businessName eg: Manage Assignment > Create New Assignment"/>
-        <Menu business="home"/>
-        {/* menu co prop là business tương ứng home, manage_user, manage_asset, manage_assignment, request_for_returning, report */}
+        <Menu business={business.HOME}/>
+        {/* menu co prop là business tương ứng HOME, MANAGE_USER, ... mn xem them trong const */}
         {/* layout mẫu mn cu v mà làm nha */}
       </div>
     );
