95eab1160c155490fb2077fe3f189b0ac665a408
CuongNC <nccuong281299@gmail.com>
2021-08-14 02:09:30 +0700

    fix error redirect between 3 page: function - first login - login, delete test component, apply some clean code, refractor project

--- src/components/FirstLogin/index.js
+++ src/components/FirstLogin/index.js
@@ -1,195 +1,195 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Navbar from "../Navbar";
 import Menu from "../Menu";
-import './FirstLogin.css'
-import {
-    Button, Form, FormGroup, Label, Input,
-    Modal, ModalHeader, ModalBody, ModalFooter
-} from "reactstrap";
-import { withRouter } from "react-router-dom";
+import "./FirstLogin.css";
+import { Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
+import { withRouter, Redirect } from "react-router-dom";
 import * as business from "../../constants/Business";
 import axios from "axios";
-import { instanceOf } from 'prop-types';
-import { withCookies, Cookies } from 'react-cookie';
-import { put } from '../../httpHelper';
+import { instanceOf } from "prop-types";
+import { withCookies, Cookies } from "react-cookie";
+import { put } from "../../httpHelper";
+import * as URL from "../../constants/URL";
+import * as role from "../../constants/Business";
 
 class index extends Component {
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired,
+  };
 
-    static propTypes = {
-        cookies: instanceOf(Cookies).isRequired
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDisabled: true,
+      password: "",
+      isFailed: false,
+      messageFail: "",
+      user: this.props.cookies.get("user") || "",
     };
+  }
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isDisabled: true,
-            password: '',
-            isFailed: false,
-            messageFail: '',
-            user: this.props.cookies.get('user') || ''
-        }
-    }
-
-    changePassword(e) {
-        e.preventDefault();
-        if (this.state.user.role === 'ROLE_ADMIN') {
-            let url = "admin/password/first";
-            let body = {
-                staffCode: `${this.state.user.staffCode}`,
-                newPassword: e.target.password.value
+  changePassword(e) {
+    e.preventDefault();
+    if (this.state.user.role === "ROLE_ADMIN") {
+      let url = "admin/password/first";
+      let body = {
+        staffCode: `${this.state.user.staffCode}`,
+        newPassword: e.target.password.value,
+      };
+      put(url, body)
+        .then((response) => {
+          if (response.status === 200) {
+            if (response.data.successCode === "CHANGE_PASSWORD_SUCCESS") {
+              this.props.cookies.set("user", JSON.stringify(response.data.data), { path: "/" });
+              this.props.history.push("/admin");
             }
-            put(url, body)
-                .then(response => {
-                    if (response.status === 200) {
-                        if (response.data.successCode === 'CHANGE_PASSWORD_SUCCESS') {
-                            this.props.cookies.set('user', JSON.stringify(response.data.data), { path: "/" })
-                            this.props.history.push("/admin");
-                        }
-                    }
-                })
-                .catch(err => {
-                    if (err.response) {
-                        switch (err.response.data.message) {
-                            case 'USER_NOT_FOUND':
-                                this.setState({ messageFail: 'User not found.' });
-                                break;
-                            case 'USER_IS_DISABLED':
-                                this.setState({ messageFail: 'User is disabled.' });
-                                break;
-                            case 'PASSWORD_IS_EMPTY':
-                                this.setState({ messageFail: 'Password is empty.' });
-                                break;
-                            case 'SAME_PASSWORD':
-                                this.setState({ messageFail: 'New password must be different the old one.' });
-                                break;
-                            default:
-                                this.setState({ messageFail: 'Error to change password.' });
-                        }
-                    }
-                    else {
-                        this.setState({ messageFail: 'Fail to change password.' });
-                    }
-                    this.setState({ isFailed: true });
-                })
-        }
-        else if (this.state.user.role === 'ROLE_USER') {
-            let url = "user/password/first";
-            let body = {
-                staffCode: `${this.state.user.staffCode}`,
-                newPassword: e.target.password.value
+          }
+        })
+        .catch((err) => {
+          if (err.response) {
+            switch (err.response.data.message) {
+              case "USER_NOT_FOUND":
+                this.setState({ messageFail: "User not found." });
+                break;
+              case "USER_IS_DISABLED":
+                this.setState({ messageFail: "User is disabled." });
+                break;
+              case "PASSWORD_IS_EMPTY":
+                this.setState({ messageFail: "Password is empty." });
+                break;
+              case "SAME_PASSWORD":
+                this.setState({ messageFail: "New password must be different the old one." });
+                break;
+              default:
+                this.setState({ messageFail: "Error to change password." });
             }
-            put(url, body)
-                .then(response => {
-                    if (response.status === 200) {
-                        if (response.data.successCode === 'CHANGE_PASSWORD_SUCCESS') {
-                            this.props.cookies.set('user', JSON.stringify(response.data.data), { path: "/" })
-                            this.props.history.push("/user");
-                        }
-                    }
-                })
-                .catch(err => {
-                    if (err.response) {
-                        switch (err.response.data.message) {
-                            case 'USER_NOT_FOUND':
-                                this.setState({ messageFail: 'User not found.' });
-                                break;
-                            case 'USER_IS_DISABLED':
-                                this.setState({ messageFail: 'User is disabled.' });
-                                break;
-                            case 'PASSWORD_IS_EMPTY':
-                                this.setState({ messageFail: 'Password is empty.' });
-                                break;
-                            case 'SAME_PASSWORD':
-                                this.setState({ messageFail: 'New password must be different the old one.' });
-                                break;
-                            default:
-                                this.setState({ messageFail: 'Error to change password.' });
-                        }
-                    }
-                    else {
-                        this.setState({ messageFail: 'Fail to change password.' });
-                    }
-                    this.setState({ isFailed: true });
-                })
-        }
-    }
-
-    handleChange(e, key) {
-        this.setState({ [key]: e.target.value }, () => {
-            if (this.state.password.trim().length > 0) {
-                this.setState({ isDisabled: false })
+          } else {
+            this.setState({ messageFail: "Fail to change password." });
+          }
+          this.setState({ isFailed: true });
+        });
+    } else if (this.state.user.role === "ROLE_USER") {
+      let url = "user/password/first";
+      let body = {
+        staffCode: `${this.state.user.staffCode}`,
+        newPassword: e.target.password.value,
+      };
+      put(url, body)
+        .then((response) => {
+          if (response.status === 200) {
+            if (response.data.successCode === "CHANGE_PASSWORD_SUCCESS") {
+              this.props.cookies.set("user", JSON.stringify(response.data.data), { path: "/" });
+              this.props.history.push("/user");
             }
-            else {
-                this.setState({ isDisabled: true })
+          }
+        })
+        .catch((err) => {
+          if (err.response) {
+            switch (err.response.data.message) {
+              case "USER_NOT_FOUND":
+                this.setState({ messageFail: "User not found." });
+                break;
+              case "USER_IS_DISABLED":
+                this.setState({ messageFail: "User is disabled." });
+                break;
+              case "PASSWORD_IS_EMPTY":
+                this.setState({ messageFail: "Password is empty." });
+                break;
+              case "SAME_PASSWORD":
+                this.setState({ messageFail: "New password must be different the old one." });
+                break;
+              default:
+                this.setState({ messageFail: "Error to change password." });
             }
+          } else {
+            this.setState({ messageFail: "Fail to change password." });
+          }
+          this.setState({ isFailed: true });
         });
     }
+  }
 
-    render() {
-        return (
-            <div>
-                {
-                    this.state.user === '' &&
-                    this.props.history.push('/')
-                }
-                <Navbar businessName="Home" />
-                <div id="Login_the_first_time">
-                    <div id="Login_mph" className="Login">
-                        <svg className="header_mpi">
-                            <rect id="header_mpi" rx={10} ry={10} x={0} y={0} width="465.649" height={64}>
-                            </rect>
-                        </svg>
-                        <svg className="background_mpj" viewBox="0 0 465.648 228.959">
-                            <path id="background_mpj" d="M 0 0 L 465.6484680175781 0 L 465.6484680175781 222.9495544433594 C 465.6484680175781 226.2684936523438 461.30517578125 228.958984375 455.9474182128906 228.958984375 L 9.701008796691895 228.958984375 C 4.343289375305176 228.958984375 0 226.2684936523438 0 222.9495544433594 L 0 0 Z">
-                            </path>
-                        </svg>
-                        <Form inline onSubmit={(e) => this.changePassword(e)}>
-                            <div id="Save_btn_mpk" className="Save_btn">
-                                <div id="Primary__#CF2338_mpl" className="Save_btn">
-                                    <Button className=" btn-block" color="danger" disabled={this.state.isDisabled}>
-                                        Save
-                                    </Button>
-                                </div>
-                            </div>
-                            <div id="PW_mpn">
-                                <div id="New_password_">
+  handleChange(e, key) {
+    this.setState({ [key]: e.target.value }, () => {
+      if (this.state.password.trim().length > 0) {
+        this.setState({ isDisabled: false });
+      } else {
+        this.setState({ isDisabled: true });
+      }
+    });
+  }
 
-                                    <FormGroup className="mb-2 mr-sm-2 mb-sm-0"><span style={{ color: 'rgba(233,66,77,1)' }}>*</span>
-                                        <Label className="mr-sm-2" for="pw">New password</Label>
-                                        <Input
-                                            type="password"
-                                            name="password"
-                                            id="pw"
-                                            onChange={(e) => this.handleChange(e, "password")} />
-                                    </FormGroup>
-
-                                </div>
-                            </div>
-                        </Form>
-                        <div id="username_mpt">
-                            <div id="This_is_the_first_time_you_log">
-                                <span>This is the first time you logged in.<br />You have to change your password to continue.</span>
-                            </div>
-                        </div>
-                        <div id="Change_password_mpv">
-                            <span>Change password</span>
-                        </div>
-                    </div>
-                </div>
-                <Modal isOpen={this.state.isFailed} >
-                    <ModalHeader>Notice</ModalHeader>
-                    <ModalBody>
-                        <p style={{ color: 'red' }}>
-                            {this.state.messageFail}
-                        </p>
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="primary" onClick={() => this.setState({ isFailed: false })}>Close</Button>
-                    </ModalFooter>
-                </Modal>
-            </div>
-        )
+  componentDidMount() {
+    if (this.state.user.role === role.ROLE_ADMIN && this.state.user.firstLogin === true) {
+      this.props.history.push(URL.HOME_ADMIN);
+    } else if (this.state.user.role === role.ROLE_STAFF && this.state.user.firstLogin === true) {
+      this.props.history.push(URL.HOME_USER);
     }
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar businessName="Home" />
+        <div id="Login_the_first_time">
+          <div id="Login_mph" className="Login">
+            <svg className="header_mpi">
+              <rect id="header_mpi" rx={10} ry={10} x={0} y={0} width="465.649" height={64}></rect>
+            </svg>
+            <svg className="background_mpj" viewBox="0 0 465.648 228.959">
+              <path
+                id="background_mpj"
+                d="M 0 0 L 465.6484680175781 0 L 465.6484680175781 222.9495544433594 C 465.6484680175781 226.2684936523438 461.30517578125 228.958984375 455.9474182128906 228.958984375 L 9.701008796691895 228.958984375 C 4.343289375305176 228.958984375 0 226.2684936523438 0 222.9495544433594 L 0 0 Z"
+              ></path>
+            </svg>
+            <Form inline onSubmit={(e) => this.changePassword(e)}>
+              <div id="Save_btn_mpk" className="Save_btn">
+                <div id="Primary__#CF2338_mpl" className="Save_btn">
+                  <Button className=" btn-block" color="danger" disabled={this.state.isDisabled}>
+                    Save
+                  </Button>
+                </div>
+              </div>
+              <div id="PW_mpn">
+                <div id="New_password_">
+                  <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
+                    <span style={{ color: "rgba(233,66,77,1)" }}>*</span>
+                    <Label className="mr-sm-2" for="pw">
+                      New password
+                    </Label>
+                    <Input type="password" name="password" id="pw" onChange={(e) => this.handleChange(e, "password")} />
+                  </FormGroup>
+                </div>
+              </div>
+            </Form>
+            <div id="username_mpt">
+              <div id="This_is_the_first_time_you_log">
+                <span>
+                  This is the first time you logged in.
+                  <br />
+                  You have to change your password to continue.
+                </span>
+              </div>
+            </div>
+            <div id="Change_password_mpv">
+              <span>Change password</span>
+            </div>
+          </div>
+        </div>
+        <Modal isOpen={this.state.isFailed}>
+          <ModalHeader>Notice</ModalHeader>
+          <ModalBody>
+            <p style={{ color: "red" }}>{this.state.messageFail}</p>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={() => this.setState({ isFailed: false })}>
+              Close
+            </Button>
+          </ModalFooter>
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default withCookies(withRouter(index));
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,11 +1,12 @@
 import React, { Component, useEffect, useState } from "react";
 import "./Login.css";
-import { useCookies } from 'react-cookie';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import { useCookies } from "react-cookie";
+import { Button, Form, FormGroup, Label, Input } from "reactstrap";
 import { Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import axios from "axios";
-import {post} from '../../httpHelper';
-import { withRouter, useHistory } from "react-router-dom";
+import { post } from "../../httpHelper";
+import { withRouter, useHistory, Redirect } from "react-router-dom";
+import * as URL from "../../constants/URL";
 
 const Login = (props) => {
   const [cookies, setCookie] = useCookies(["user"]);
@@ -25,42 +26,39 @@ const Login = (props) => {
     let url = "signin";
     let body = {
       username,
-      password
-    }
+      password,
+    };
     post(url, body)
-    .then(response => {
-      if (response.status === 200) {
-        if (response.data.successCode === "USER_LOGIN_SUCCESS") {
-          setCookie("user", JSON.stringify(response.data.data), {
-            path: "/",
-          });
-          localStorage.setItem("jwtToken", response.data.data.token);
-          localStorage.setItem("locationId", response.data.data.idLocation);
-          // props.onLogin();
-          if(response.data.data.firstLogin === false) {
-            history.push("/first");
-          }
-          if(response.data.data.firstLogin === true 
-            && response.data.data.role === 'ROLE_ADMIN')
-          {
-            history.push("/admin");
-          }
-          if(response.data.data.firstLogin === true 
-            && response.data.data.role === 'ROLE_USER')
-          {
-            history.push("/user");
+      .then((response) => {
+        if (response.status === 200) {
+          if (response.data.successCode === "USER_LOGIN_SUCCESS") {
+            setCookie("user", JSON.stringify(response.data.data), {
+              path: "/",
+            });
+            localStorage.setItem("jwtToken", response.data.data.token);
+            localStorage.setItem("locationId", response.data.data.idLocation);
+            // props.onLogin();
+            if (response.data.data.firstLogin === false) {
+              history.push("/first");
+            }
+            if (response.data.data.firstLogin === true && response.data.data.role === "ROLE_ADMIN") {
+              history.push("/admin");
+            }
+            if (response.data.data.firstLogin === true && response.data.data.role === "ROLE_USER") {
+              history.push("/user");
+            }
           }
         }
-      }
-    })
-    .catch(err => {
-      console.log(err);
-      setIsFailed(true);
-    })
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsFailed(true);
+      });
   };
 
   return (
     <div id="Login" className="Login">
+      {Object.keys(cookies).length === 0 && cookies.constructor === Object ? "" : <Redirect to={URL.FIRST_LOGIN} />}
       <div id="Welcome_to_Online_Asset_Manage">
         <p>Welcome to Online Asset Management</p>
       </div>
@@ -68,37 +66,21 @@ const Login = (props) => {
         <form>
           <div className="login-data">
             <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              name="username"
-              value={username}
-              onChange={({ target }) => setUsername(target.value)}
-            />
+            <input type="text" name="username" value={username} onChange={({ target }) => setUsername(target.value)} />
           </div>
           <div className="login-data">
             <label htmlFor="password">Password</label>
-            <input
-              type="password"
-              name="password"
-              value={password}
-              onChange={({ target }) => setPassword(target.value)}
-            />
+            <input type="password" name="password" value={password} onChange={({ target }) => setPassword(target.value)} />
           </div>
         </form>
-        <Button
-          className="login-btn"
-          disabled={isDisabled}
-          onClick={handleLogin}
-        >
+        <Button className="login-btn" disabled={isDisabled} onClick={handleLogin}>
           Login
         </Button>
       </div>
       <Modal isOpen={isFailed}>
         <ModalHeader>Notice</ModalHeader>
         <ModalBody>
-          <p style={{ color: "red" }}>
-            Username or Password is incorrect. Please try again.
-          </p>
+          <p style={{ color: "red" }}>Username or Password is incorrect. Please try again.</p>
         </ModalBody>
         <ModalFooter>
           <Button color="primary" onClick={() => setIsFailed(false)}>
@@ -108,6 +90,6 @@ const Login = (props) => {
       </Modal>
     </div>
   );
-}
+};
 
 export default withRouter(Login);
